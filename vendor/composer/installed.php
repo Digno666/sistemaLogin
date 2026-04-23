ad68a7429f69490243fa88c3f3a5f85808991b90
Digno666 <dignoarmandovargasleon@gmail.com>
2026-04-22 21:15:53 -0400

    Generar composer.lock para Aikido

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'digno666/sistema-login',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '454d5b7fc5a73d6dad3359feb4cfbe9cf8a1846f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'digno666/sistema-login' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '454d5b7fc5a73d6dad3359feb4cfbe9cf8a1846f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
